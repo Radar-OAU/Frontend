b3d8d3d3110f2cbac361ce6be1ee260fd2f1a07a
oluwatobi <luponet07@gmail.com>
2026-01-10 16:25:30 +0100

    some fixes

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,8 +2,8 @@ import { Plus_Jakarta_Sans, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { GoogleAuthProvider } from "@/components/GoogleAuthProvider";
 import { Toaster } from "react-hot-toast";
-// import Header from "@/components/Header";
-// import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 import { ThemeProvider } from "@/components/theme-provider";
 
